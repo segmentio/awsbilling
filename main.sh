352ee8340469e9054903aa13cf674a1b986f120c
Tido Carriero <tido@segment.com>
2016-11-22 12:46:08 -0800

    Better sleep strategy to implement cron-like behavior

--- main.sh
+++ main.sh
@@ -1,3 +1,18 @@
+#
+# This is some logic that causes this to wait until 11am UTC (3am Pacific)
+# for this to run. Eventually, this should be a cron once ECS supports cron.
+#
+NOW=`date -d "now" +%s`
+TOMORROW_11AM=`date -d "tomorrow 11am" +%s`
+SLEEP_TIME=$(($TOMORROW_11AM - $NOW))
+
+echo "Sleeping $SLEEP_TIME seconds until 11am UTC tomorrow..."
+sleep $SLEEP_TIME
+
+#
+# And this is where the real script begins!
+#
+
 echo "Running node import_month_to_date.js..."
 node import_month_to_date.js --debug --no-vacuum
 echo "Finished running import_month_to_date.js..."
@@ -5,5 +20,3 @@ echo "Finished running import_month_to_date.js..."
 echo "Running node import_finalized.js..."
 node import_finalized.js --debug --no-vacuum
 echo "Finished running import_finalized.js..."
-
-sleep 82800
